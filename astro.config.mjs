21a6e8140ed8f8ef86c5c450a2ae0adc00e0f8ff
Sveta Co <s.costromina@gmail.com>
2025-06-14 17:58:58 +0300

    app_page + assets, Picture, Icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [react()],
   markdown: {
     shikiConfig: {
       theme: "nord",
